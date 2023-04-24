352891e52be22010db31ceb4697eb5f0e3639e17
rbhaskar99 <bhaskar.r@northeastern.edu>
2023-04-24 00:28:13 -0400

    First commit

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # Basic framework needed for flask to create a webpage
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return"My name is potet"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
