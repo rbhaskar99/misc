685317411d315c5fc8a0c2586e015435b25be29a
rbhaskar99 <bhaskar.r@northeastern.edu>
2023-04-26 10:55:08 -0400

    Second commit

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 # Basic framework needed for flask to create a webpage
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
 
 @app.route('/')
 def index():
